a5b78c537adf5944e13be55ac323dc56427328aa
Jasika-Kumari <jasikakumari17@gmail.com>
2025-01-07 22:50:41 +0530

    router setup

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Container,Button,Row} from 'reactstrap'
+import {NavLink, Link} from 'react-router-dom'
 
 const Header = () => {
   return (
--- frontend/src/component/Layout/Layout.jsx
+++ frontend/src/component/Layout/Layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Header from './../Header/Header';
+import Routers from '../../router/Routers';
+import Footer from './../Footer/Footer';
 const Layout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header/>
+      <Routers/>
+      <Footer/>
+    </>
   )
 }
 
