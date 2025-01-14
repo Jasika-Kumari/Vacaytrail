f7138e5818269717c1273f51d26b40484152f9fe
Jasika-Kumari <jasikakumari17@gmail.com>
2025-01-14 22:45:00 +0530

    tourdetails css

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -22,10 +22,36 @@ const TourDetails = () => {
                 <div className='d-flex align-items-center gap-5'>
                 <span className='tour__rating d-flex align-items-center gap-1'>
            <i class="ri-star-fill" style={{'color':"var(--secondary-color)"}}></i>{calculateAvgRating===0?null:avgRating}
-           {totalRating===0?'Not rated':<span>({reviews.length})</span>}
+           {totalRating===0?('Not rated'):(<span>({reviews?.length})</span>)}
            </span>
+                <span>
+                <i class="ri-map-pin-user-fill"></i>{address}
+                </span>
+           
                 </div>
+                <div className="tour__extra-details">
+                  <span>
+                  <i class="ri-map-pin-2-line"></i>{city}
+                  </span>
+                  <span>
+                  <i class="ri-money-dollar-circle-line"></i>${price}/per person
+                  </span>
+                  <span>
+                  <i class="ri-group-line"></i>{maxGroupSize}
+                  </span>
+                </div>
+                <h5>Description</h5>
+                <p>{desc}</p>
               </div>
+             <div className='tour__reviews mt-4'>
+              <h4>Reviews ({reviews?.length}reviews)</h4>
+              <Form>
+                <div className='d-flex align-items-center gap-3'>
+
+                </div>
+              </Form>
+             </div>
+
              </div>
           </Col>
         </Row>
