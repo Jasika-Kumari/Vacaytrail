c3343d9600b69e4cdcb7273533deee33d56f655c
Jasika-Kumari <jasikakumari17@gmail.com>
2025-01-25 22:26:44 +0530

    backend start

--- frontend/src/shared/Newsletter.jsx
+++ frontend/src/shared/Newsletter.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import './newsletter.css' 
-import {Container,Row,Col} from 'reactstrap'
-import maleTourist from '../assets/images/male-tourist.png'
+import React from 'react';
+import './newsletter.css'; 
+import { Container, Row, Col } from 'reactstrap';
+import maleTourist from '../assets/images/male-tourist.png';
+
 const Newsletter = () => {
   return (
     <section className='newsletter'>
       <Container>
         <Row>
-            <Col lg='6'>
+          <Col lg='6'>
             <div className="newsletter__content">
-                <h2>Subscribe now to get traveling information.</h2>
-                <div className="newsletter__input">
-                    <input type="email" placeholder='Enter your email'/>
-                    <button className='btn newsletter__btn'></button>
-
-                </div>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis corrupti, aliquid asperiores tempore facere eius cupiditate harum maxime consequuntur reiciendis amet nam aliquam, natus expedita ab assumenda molestiae ipsa. Adipisci!</p>
+              <h2>Join Our Adventure Community!</h2>
+              <p>Subscribe now to receive the latest travel tips, exclusive offers, and inspiring stories from around the world.</p>
+              <p>Don't miss out on our exciting updates and travel guides. Your next adventure awaits!</p>
+              <div className="newsletter__input">
+                <input type="email" placeholder='Enter your email' />
+                <button className='btn newsletter__btn'>Subscribe</button>
+              </div>
+             
             </div>
-            </Col>
-            <Col lg='6'>
+          </Col>
+          <Col lg='6'>
             <div className="newsletter__img">
-                <img src={maleTourist} alt=""/>
+              <img src={maleTourist} alt="Male Tourist" />
             </div>
-            </Col>
+          </Col>
         </Row>
       </Container>
     </section>
-  )
-  
+  );
 }
 
-export default Newsletter
+export default Newsletter;
