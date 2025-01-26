fd16f771f63be5d45e03e90b4a7623f6247fd40f
Jasika-Kumari <jasikakumari17@gmail.com>
2025-01-26 11:04:48 +0530

    backend

---
+++ backend/controllers/tourController.js
@@ -0,0 +1,16 @@
+import Tour from '../models/Tour.js'
+//create new tour
+export const createTour=async (req,res)=>{
+    const newTour=new Tour(req.body)
+    try{
+        const savedTour=await newTour.save()
+        res.status(200).json({success:true, message:'Successfully created', data:savedTour})
+    }catch(err){
+        console.error(err); // Log the error for debugging
+        res.status(500).json({
+            success: false,
+            message: 'Failed to create tour',
+           
+        });
+    }
+}
