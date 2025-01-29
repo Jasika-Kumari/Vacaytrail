df1071d2dcb3cd32e3fa44a075558cffdc06399e
Jasika-Kumari <jasikakumari17@gmail.com>
2025-01-29 21:33:28 +0530

    backend

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -16,35 +16,46 @@ export const createTour=async (req,res)=>{
 }
 //update tour
 export const updateTour=async(req,res)=>{
-    try{
-
-    }catch(err){
-
-    }
-};
-export const deleteTour=async(req,res)=>{
     const id=req.params.id
     try{
-       const updatedTour=await Tour.findByIdAndUpdate(id,{
-        $set:req.body
-       },{new:true})
-       res.status(200).json({success:true, message:'Successfully updated', data:updatedTour});
-       
+        const updatedTour=await Tour.findByIdAndUpdate(id,{
+            $set:req.body
+           },{new:true})
+           res.status(200).json({success:true, message:'Successfully updated', data:'updatedTour'});   
+     
     }catch(err){
         res.status(500).json({success:false, message:'failed to update'});
     }
 };
-export const getSingleTour=async(req,res)=>{
+export const deleteTour=async(req,res)=>{
+    const id=req.params.id;
     try{
-
+          await Tour.findByIdAndDelete(id);
+           res.status(200).json({success:true, message:'Successfully deleted'});   
+     
     }catch(err){
-
+        res.status(500).json({success:false, message:'failed to delete'});
+    }
+    
+};
+export const getSingleTour=async(req,res)=>{
+    const id=req.params.id;
+    try{
+          const tour=await Tour.findById(id);
+           res.status(200).json({success:true, message:'Successfully deleted',data:'tour'});   
+     
+    }catch(err){
+        res.status(404).json({success:false, message:'not found'});
     }
 };
 export const getAllTour=async(req,res)=>{
+    //for pagination
+    const page=parseInt(req.query.page);
+    
     try{
-
+       const tours=await Tour.find({});
+       res.status(200).json({success:true,count:tours.length, message:'Successful',data:tours})
     }catch(err){
-
+        res.status(404).json({success:false, message:'not found'});
     }
 };
