63292a3c7cce7dc240b16057e92c2a183cad61db
Jasika-Kumari <jasikakumari17@gmail.com>
2025-02-14 11:57:41 +0530

    login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {updateUser,getAllUser,getSingleUser,deleteUser} from '../controllers/userController.js'
+import {verifyUser} from '../utils/verifyToken.js'
 const router=express.Router()
 //update user
 router.put('/:id',updateUser);
 //delete  user
 router.delete('/:id',deleteUser);
 //get single user
-router.get('/:id',getSingleUser);
+router.get('/:id',verifyUser,getSingleUser);
 //get all user
 router.get('/',getAllUser);
 
